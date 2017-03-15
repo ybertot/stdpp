a7ec0da6203a1f602218f2ad7007b3f68934c566
Robbert Krebbers <mail@robbertkrebbers.nl>
2017-03-15 15:58:56 +0100

    Some missing copyright headers.

--- theories/functions.v
+++ theories/functions.v
@@ -1,3 +1,5 @@
+(* Copyright (c) 2012-2017, Coq-std++ developers. *)
+(* This file is distributed under the terms of the BSD license. *)
 From stdpp Require Export base tactics.
 Set Default Proof Using "Type".
 
