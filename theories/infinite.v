3a262d020c0181ca5f390464ec01b7a478c88f86
Johannes Kloos <jkloos@mpi-sws.org>
2017-10-31 11:40:58 +0100

    Proofs of infinity and Fresh instances.
    We prove that various types are infinite, notably:
    - nat, N, positive and Z;
    - string (using pretty-printing of nat);
    - option, with an infinite element type;
    - list, with an inhabited element type.
    
    Furthermore, we instantiate Fresh for strings.

---
+++ theories/infinite.v
@@ -0,0 +1,24 @@
+(* Copyright (c) 2012-2017, Coq-std++ developers. *)
+(* This file is distributed under the terms of the BSD license. *)
+From stdpp Require Import pretty fin_collections.
+
+Instance string_infinite: Infinite string := {| inject := λ x, "~" +:+ pretty x |}.
+Instance nat_infinite: Infinite nat := {| inject := id |}.
+Instance N_infinite: Infinite N := {| inject_injective := Nat2N.inj |}.
+Instance pos_infinite: Infinite positive := {| inject_injective := SuccNat2Pos.inj |}.
+Instance Z_infinite: Infinite Z := {| inject_injective := Nat2Z.inj |}.
+Instance option_infinite `{Infinite A}: Infinite (option A) := {| inject := Some ∘ inject |}.
+Program Instance list_infinite `{Inhabited A}: Infinite (list A) :=
+  {| inject := λ i, replicate i inhabitant |}.
+Next Obligation.
+Proof.
+  intros * i j eqrep%(f_equal length).
+  rewrite !replicate_length in eqrep; done.
+Qed.
+
+(** Derive fresh instances. *)
+Section StringFresh.
+  Context `{FinCollection string C, ∀ (x: string) (s: C), Decision (x ∈ s)}.
+  Global Instance string_fresh: Fresh string C := fresh_generic.
+  Global Instance string_fresh_spec: FreshSpec string C := _.
+End StringFresh.
