f5c139ed6dfc8f77540702c2601c02a836d4e190
Ralf Jung <jung@mpi-sws.org>
2019-06-20 14:40:17 +0200

    test solve_ndisj a bit

---
+++ tests/solve_ndisj.v
@@ -0,0 +1,26 @@
+From stdpp Require Import namespaces strings.
+
+Lemma test1 (N1 N2 : namespace) :
+  N1 ## N2 → ↑N1 ⊆@{coPset} ⊤ ∖ ↑N2.
+Proof. solve_ndisj. Qed.
+
+Lemma test2 (N1 N2 : namespace) :
+  N1 ## N2 → ↑N1.@"x" ⊆@{coPset} ⊤ ∖ ↑N1.@"y" ∖ ↑N2.
+Proof. solve_ndisj. Qed.
+
+Lemma test3 (N : namespace) :
+  ⊤ ∖ ↑N ⊆@{coPset} ⊤ ∖ ↑N.@"x".
+Proof. solve_ndisj. Qed.
+
+Lemma test4 (N : namespace) :
+  ⊤ ∖ ↑N ⊆@{coPset} ⊤ ∖ ↑N.@"x" ∖ ↑N.@"y".
+Proof. solve_ndisj. Qed.
+
+Lemma test5 (N1 N2 : namespace) :
+  ⊤ ∖ ↑N1 ∖ ↑N2 ⊆@{coPset} ⊤ ∖ ↑N1.@"x" ∖ ↑N2 ∖ ↑N1.@"y".
+Proof.
+  Fail solve_ndisj. (* FIXME: it should be able to solve this. *)
+  apply namespace_subseteq_difference.
+  { apply ndot_preserve_disjoint_r. set_solver. }
+  solve_ndisj.
+Qed.
