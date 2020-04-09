f5ed12e195e5835c09a64c5eff89caf2084ce565
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-04-10 01:42:49 +0200

    Add tests for equiv notation
    Extracted from https://gitlab.mpi-sws.org/iris/iris/-/merge_requests/409.

---
+++ tests/notation.v
@@ -0,0 +1,9 @@
+From stdpp Require Import base tactics.
+
+(** Test parsing of variants of [(≡)] notation. *)
+Lemma test_equiv_annot_sections `{!Equiv A, Equivalence A (≡@{A})} (x : A) :
+  x ≡@{A} x ∧ (≡@{A}) x x ∧ (x ≡.) x ∧ (.≡ x) x ∧
+  ((x ≡@{A} x)) ∧ ((≡@{A})) x x ∧ ((x ≡.)) x ∧ ((.≡ x)) x ∧
+  ( x ≡@{A} x) ∧ ( x ≡.) x ∧
+  (x ≡@{A} x ) ∧ (≡@{A} ) x x ∧ (.≡ x ) x.
+Proof. naive_solver. Qed.
