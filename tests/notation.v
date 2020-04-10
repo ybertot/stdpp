f88fe7a60279b782509a6eb00c9c0819ea7ba134
Robbert <gitlab-sws@robbertkrebbers.nl>
2020-04-10 07:57:02 +0200

    Apply suggestion to tests/notation.v

--- tests/notation.v
+++ tests/notation.v
@@ -1,7 +1,7 @@
 From stdpp Require Import base tactics.
 
 (** Test parsing of variants of [(≡)] notation. *)
-Lemma test_equiv_annot_sections `{!Equiv A, Equivalence A (≡@{A})} (x : A) :
+Lemma test_equiv_annot_sections `{!Equiv A, !Equivalence (≡@{A})} (x : A) :
   x ≡@{A} x ∧ (≡@{A}) x x ∧ (x ≡.) x ∧ (.≡ x) x ∧
   ((x ≡@{A} x)) ∧ ((≡@{A})) x x ∧ ((x ≡.)) x ∧ ((.≡ x)) x ∧
   ( x ≡@{A} x) ∧ ( x ≡.) x ∧
