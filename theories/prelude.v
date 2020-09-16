92aceef153b80d83e9b003a76416da842a2a772c
Ralf Jung <jung@mpi-sws.org>
2020-09-16 15:30:20 +0200

    fix base.v import order; use real import in prelude.v

--- theories/prelude.v
+++ theories/prelude.v
@@ -12,8 +12,4 @@ From stdpp Require Export
   list
   list_numbers
   lexico.
-
-(* "Fake" import to whitelist this file for the check that ensures we import
-this file everywhere.
 From stdpp Require Import options.
-*)
